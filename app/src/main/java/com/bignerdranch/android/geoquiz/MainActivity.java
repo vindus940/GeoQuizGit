819afbe391c222f928422c3f9a3507cc3b205d3b
vindus940 <gawrilenkomaxim@gmail.com>
2020-06-24 14:58:57 +0300

    fix

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -1,13 +1,14 @@
 package com.bignerdranch.android.geoquiz;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
-        import android.view.Gravity;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mNextButton;
     private TextView mQuestionTextView;
 
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -38,16 +39,13 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 checkAnswer(true);
-
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 checkAnswer(false);
             }
         });
@@ -57,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-
                 updateQuestion();
             }
         });
@@ -87,5 +84,5 @@ public class MainActivity extends AppCompatActivity {
         toast.setGravity(Gravity.TOP, 0, 150);
         toast.show();
 
-        }
     }
+}
---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Test.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.geoquiz;
+
+public class Test {
+    Question q = null;
+    Question q1 = new Question(0, false);
+
+    void hui1() {
+        Question q3 = new Question(0, false);
+    }
+
+    int hui2() {
+
+        return 0;
+    }
+}
